a9575b77d211480a930a688ef045ee80a1c943b1
One-djey <maisse.jeremy@sfr.fr>
2014-12-16 12:28:12 +0100

    Graphiques
    Ajout de nouveau graphique:
    -graphique aire
    -map
    
    correction de l'histogramme

--- js/exemples.js
+++ js/exemples.js
@@ -2,6 +2,8 @@
       google.load('visualization', '1.0', {'packages':['corechart']});
       google.setOnLoadCallback(Circulaire);
 	  google.setOnLoadCallback(Histogramme);
+	  google.setOnLoadCallback(Aire);
+	  google.setOnLoadCallback(Map);
 	  
 	  /*****Circulaire*****/
       function Circulaire() {
@@ -34,20 +36,60 @@
       function Histogramme() {
 
       var data = new google.visualization.arrayToDataTable([
-		['Formation','Nombre'],
-        ['Info 1', 6],
-		['Info 2', 3],
-		['MMI 1', 5],
-		['MMI 2', 1],
-		['LPs ATC', 5],
-		['LP SIL IN', 4]		
+		['Formation',''],
+        ['Info 1', 60],
+		['Info 2', 30],
+		['MMI 1', 50],
+		['MMI 2', 20],
+		['LPs ATC', 50],
+		['LP SIL IN', 40]		
       ]);
 
-      var options = {'orientation':'vertical',
-					 'hAxis':{title:'Formations', titleTextStyle:{color: '#AAAAAA'}},
+      var options = {'orientation':'horizontal',
+					// 'hAxis':{title:'Formations', titleTextStyle:{color: '#AAAAAA'}},
 					 'legend':'none',
 					 };
 
-      var chart = new google.visualization.Histogram(document.getElementById('histogramme'));
+      var chart = new google.visualization.BarChart(document.getElementById('histogramme'));
       chart.draw(data, options);
     }
+	
+	/*****Aire*****/
+	 function Aire() {
+        var data = google.visualization.arrayToDataTable([
+          ['Année', 'Arles','Marseille'],
+          ['2012',  50, 100],
+          ['2013',  110, 90],
+          ['2014',  90, 120],
+          ['2015',  120, 130]
+        ]);
+
+        var options = {
+          hAxis: {title: 'Années',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('aire'));
+        chart.draw(data, options);
+      }
+	  
+	/*****Carte*****/
+	function Map() {
+      var data = google.visualization.arrayToDataTable([
+        ['Ville',   'Population'],
+        ['Arles',  100],
+        ['Marseille', 42],
+        ['Paris', 3],
+        ['Aix',  66]
+      ]);
+
+      var options = {
+        sizeAxis: { minValue: 0, maxValue: 100 },
+        region: 'FR', // Western Europe
+        displayMode: 'markers',
+        colorAxis: {colors: ['#2222EE', '#EE2222']} // bleu au rouge
+      };
+
+      var chart = new google.visualization.GeoChart(document.getElementById('map'));
+      chart.draw(data, options);
+    };
